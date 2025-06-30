20b85d2cfa6edc63bcd6631389f04e21e47c1122
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-06-30 23:50:47 +0300

    Add led to example

--- examples/fusb302-esp32c3-arduino/main.cpp
+++ examples/fusb302-esp32c3-arduino/main.cpp
@@ -1,5 +1,6 @@
 #include "pd.h"
 #include "logger.hpp"
+#include "blinker.hpp"
 
 //pd::Fusb302Esp32Arduino fusb302;
 pd::Sink sink;
@@ -9,9 +10,27 @@ pd::PE pe(sink, prl, driver);
 pd::TC tc(sink, driver);
 pd::DPM dpm(sink, pe);
 
+Blinker<LedDriver> blinker;
+
 
 void setup() {
     logger_start();
+    blinker.start();
+    // LED animation example
+    blinker.loop({ {{255,0,0}, 1000}, {{0,255,0}, 1000}, {{0,0,255}, 1000}, {{128,128,128}, 1000} });
+
+    //
+    // Devboard specific setup, not PD related
+    //
+
+    // FAN off.
+    pinMode(21, OUTPUT); digitalWrite(21, LOW);
+    // BUZZER off.
+    pinMode(10, OUTPUT); digitalWrite(10, LOW);
+    pinMode(8, OUTPUT); digitalWrite(8, LOW);
+    // Heater off.
+    pinMode(20, OUTPUT); digitalWrite(20, LOW);
+
     LOG_INFO("Setup complete");
 }
 
