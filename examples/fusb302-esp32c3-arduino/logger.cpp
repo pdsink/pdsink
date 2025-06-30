a81f866cb771138a72a6a34021d13efac8d08a78
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-06-30 23:48:42 +0300

    Use idf for logging

--- examples/fusb302-esp32c3-arduino/logger.cpp
+++ examples/fusb302-esp32c3-arduino/logger.cpp
@@ -1,11 +1,16 @@
-#include <Arduino.h>
+//#include <Arduino.h>
 #include "logger.hpp"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "hal/usb_serial_jtag_ll.h"
+#include "rom/ets_sys.h"
 
 static jetlog::RingBuffer<1024*10> ringBuffer;
 
 Logger logger(ringBuffer);
 jetlog::Reader<> logReader(ringBuffer);
 
+/*
 void logger_start() {
     xTaskCreate([](void* pvParameters) {
         (void)pvParameters;
@@ -25,6 +30,29 @@ void logger_start() {
                 outputBuffer.clear();
             }
             // If there is no data, wait a bit before trying again.
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
+    }, "LogOutputTask", 1024 * 4, NULL, 0, NULL);
+}
+*/
+
+// Use idf api for output. It's connection detector is more robust for re-uploads.
+void logger_start() {
+    xTaskCreate([](void* pvParameters) {
+        (void)pvParameters;
+
+        etl::string<1024> outputBuffer{};
+
+        // Wait until usb serial ready, or startup messages will be lost
+        while(!usb_serial_jtag_ll_txfifo_writable()) {
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
+
+        while (true) {
+            while (logReader.pull(outputBuffer)) {
+                ets_printf("%s\n", outputBuffer.c_str());
+                outputBuffer.clear();
+            }
             vTaskDelay(pdMS_TO_TICKS(10));
         }
     }, "LogOutputTask", 1024 * 4, NULL, 0, NULL);
