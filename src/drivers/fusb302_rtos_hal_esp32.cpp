d668ea5085ce790fd5213a895bf206e6de241cc8
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-07-04 14:01:47 +0300

    hal polish

--- src/drivers/fusb302_rtos_hal_esp32.cpp
+++ src/drivers/fusb302_rtos_hal_esp32.cpp
@@ -14,6 +14,7 @@ void Fusb302RtosHalEsp32::init_timer() {
     esp_timer_create_args_t timer_args = {
         .callback = [](void* arg) {
             auto* self = static_cast<Fusb302RtosHalEsp32*>(arg);
+            if (self->msg_router == nullptr) { return; }
             self->msg_router->receive(MsgHal_Timer{});
         },
         .arg = this,
@@ -26,26 +27,51 @@ void Fusb302RtosHalEsp32::init_timer() {
 }
 
 void Fusb302RtosHalEsp32::init_pins() {
+    // i2c
+    i2c_config_t conf = {};
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = sda_io_pin;
+    conf.scl_io_num = scl_io_pin;
+    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
+    conf.master.clk_speed = 400000;
+
+    ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
+    ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0));
+
+    // fusb302 interrupt pin
     gpio_config_t io_conf = {};
     io_conf.intr_type = GPIO_INTR_NEGEDGE;
     io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL << intPin);
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << int_io_pin);
+
     gpio_config(&io_conf);
 
     auto handler = [](void* arg) -> void {
         auto* self = static_cast<Fusb302RtosHalEsp32*>(arg);
+        if (self->msg_router == nullptr) { return; }
         self->msg_router->receive(MsgHal_Interrupt{});
     };
 
     gpio_install_isr_service(0);
-    gpio_isr_handler_add((gpio_num_t)intPin, handler, this);
+    ESP_ERROR_CHECK(gpio_isr_handler_add(int_io_pin, handler, this));
 }
 
 void Fusb302RtosHalEsp32::start() {
+    if (started) { return; }
+    started = true;
+
     init_timer();
     init_pins();
+}
+
+Fusb302RtosHalEsp32::~Fusb302RtosHalEsp32() {
+    if (started) {
+        esp_timer_stop(timer_handle);
+        esp_timer_delete(timer_handle);
+        gpio_isr_handler_remove(int_io_pin);
+        i2c_driver_delete(I2C_NUM_0);
+    }
 }
 
 uint64_t Fusb302RtosHalEsp32::get_timestamp() {
@@ -53,13 +79,15 @@ uint64_t Fusb302RtosHalEsp32::get_timestamp() {
 }
 
 bool Fusb302RtosHalEsp32::is_interrupt_active() {
-    return gpio_get_level(intPin) == 0; // active low
+    return gpio_get_level(int_io_pin) == 0; // active low
 }
 
 static constexpr int I2C_TIMEOUT_MS = 10;
 static_assert(pdMS_TO_TICKS(I2C_TIMEOUT_MS) > 0, "Too slow FreeRTOS tick rate, should be 1ms or faster");
 
 bool Fusb302RtosHalEsp32::read(uint8_t reg, uint8_t *data, uint32_t size) {
+    if (!started) return false;
+
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (fusb302::DeviceID::addr << 1) | I2C_MASTER_WRITE, true);
@@ -74,6 +102,8 @@ bool Fusb302RtosHalEsp32::read(uint8_t reg, uint8_t *data, uint32_t size) {
 }
 
 bool Fusb302RtosHalEsp32::write(uint8_t reg, uint8_t *data, uint32_t size) {
+    if (!started) return false;
+
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (fusb302::DeviceID::addr << 1) | I2C_MASTER_WRITE, true);
--- src/drivers/fusb302_rtos_hal_esp32.h
+++ src/drivers/fusb302_rtos_hal_esp32.h
@@ -10,7 +10,9 @@ namespace fusb302 {
 
 class Fusb302RtosHalEsp32 : public IFusb302RtosHal {
 public:
-    Fusb302RtosHalEsp32() : intPin{GPIO_NUM_7} {}
+    Fusb302RtosHalEsp32() :
+        sda_io_pin{GPIO_NUM_5}, scl_io_pin{GPIO_NUM_6}, int_io_pin{GPIO_NUM_7}
+    {}
     void set_msg_router(etl::imessage_router& router) { msg_router = &router; };
     void start() override;
     uint64_t get_timestamp() override;
@@ -18,10 +20,16 @@ public:
     bool write(uint8_t reg, uint8_t *data, uint32_t size) override;
     bool is_interrupt_active() override;
 
+    ~Fusb302RtosHalEsp32();
+
 protected:
-    gpio_num_t intPin;
+    gpio_num_t sda_io_pin;
+    gpio_num_t scl_io_pin;
+    gpio_num_t int_io_pin;
+
     etl::imessage_router* msg_router{nullptr};
     esp_timer_handle_t timer_handle;
+    bool started{false};
 
     virtual void init_timer();
     virtual void init_pins();
