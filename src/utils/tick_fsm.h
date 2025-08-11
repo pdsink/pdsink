04a475629259110d81fd88142a853dcc95e3487e
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-08-11 13:35:39 +0300

    moor fsm

---
+++ src/utils/tick_fsm.h
@@ -0,0 +1,194 @@
+#pragma once
+
+#include <etl/fsm.h>
+#include <etl/type_traits.h>
+#include <etl/integral_limits.h>
+#include <stddef.h>
+
+namespace etl_ext {
+
+namespace tfsm_details {
+    template<typename...>
+    struct first_type;
+
+    template<typename First, typename... Rest>
+    struct first_type<First, Rest...> {
+        using type = First;
+    };
+
+    // Check all states have same FSMType
+    template<typename ExpectedFSM, typename... States>
+    struct check_fsm_types;
+
+    template<typename ExpectedFSM>
+    struct check_fsm_types<ExpectedFSM> {
+        static constexpr bool value = true;
+    };
+
+    template<typename ExpectedFSM, typename First, typename... Rest>
+    struct check_fsm_types<ExpectedFSM, First, Rest...> {
+        static constexpr bool value =
+            etl::is_same<ExpectedFSM, typename First::FSMType>::value &&
+            check_fsm_types<ExpectedFSM, Rest...>::value;
+    };
+
+    // Check state IDs are sequential from 0
+    template<size_t Expected, typename... States>
+    struct check_state_ids;
+
+    template<size_t Expected>
+    struct check_state_ids<Expected> {
+        static constexpr bool value = true;
+    };
+
+    template<size_t Expected, typename First, typename... Rest>
+    struct check_state_ids<Expected, First, Rest...> {
+        static constexpr bool value =
+            (First::STATE_ID == Expected) &&
+            check_state_ids<Expected + 1, Rest...>::value;
+    };
+}
+
+template<typename FSM, typename Derived, etl::fsm_state_id_t StateID>
+class tick_fsm_state {
+public:
+    using FSMType = FSM;
+    static constexpr etl::fsm_state_id_t STATE_ID = StateID;
+
+    static constexpr auto No_State_Change = etl::ifsm_state::No_State_Change;
+    static constexpr auto Self_Transition = etl::ifsm_state::Self_Transition;
+    static constexpr etl::fsm_state_id_t ID(etl::fsm_state_id_t e){
+        return static_cast<etl::fsm_state_id_t>(e);
+    }
+
+    static etl::fsm_state_id_t on_enter_state(FSMType& fsm) {
+        return Derived::on_enter_state(fsm);
+    }
+
+    static etl::fsm_state_id_t on_run_state(FSMType& fsm) {
+        return Derived::on_run_state(fsm);
+    }
+
+    static void on_exit_state(FSMType& fsm) {
+        Derived::on_exit_state(fsm);
+    }
+};
+
+template<typename... States>
+struct tick_fsm_state_pack {
+    using FirstState = typename tfsm_details::first_type<States...>::type;
+    using FSMType = typename FirstState::FSMType;
+
+    // Compile-time checks
+    static_assert(sizeof...(States) > 0, "State pack cannot be empty");
+    static_assert(tfsm_details::check_fsm_types<FSMType, States...>::value,
+                  "All states must have the same FSMType");
+    static_assert(tfsm_details::check_state_ids<0, States...>::value,
+                  "State IDs must be sequential starting from 0");
+    static_assert((sizeof...(States) - 1) <= etl::integral_limits<etl::fsm_state_id_t>::max,
+                  "last state id overflow");
+    static_assert(!etl::integral_limits<etl::fsm_state_id_t>::is_signed,
+                  "fsm_state_id_t must be unsigned");
+
+    using on_enter_fn = etl::fsm_state_id_t(*)(FSMType&);
+    using on_run_fn = etl::fsm_state_id_t(*)(FSMType&);
+    using on_exit_fn = void(*)(FSMType&);
+
+    static constexpr size_t state_count = sizeof...(States);
+
+    static const on_enter_fn* get_enter_table() {
+        static const on_enter_fn table[state_count] = { &States::on_enter_state... };
+        return table;
+    }
+
+    static const on_run_fn* get_run_table() {
+        static const on_run_fn table[state_count] = { &States::on_run_state... };
+        return table;
+    }
+
+    static const on_exit_fn* get_exit_table() {
+        static const on_exit_fn table[state_count] = { &States::on_exit_state... };
+        return table;
+    }
+
+    static constexpr size_t get_state_count() {
+        return state_count;
+    }
+};
+
+template<typename FSMImpl>
+class tick_fsm {
+public:
+    using on_enter_fn = etl::fsm_state_id_t(*)(FSMImpl&);
+    using on_run_fn = etl::fsm_state_id_t(*)(FSMImpl&);
+    using on_exit_fn = void(*)(FSMImpl&);
+
+private:
+    const on_enter_fn* enter_table = nullptr;
+    const on_run_fn* run_table = nullptr;
+    const on_exit_fn* exit_table = nullptr;
+    size_t state_count = 0;
+    etl::fsm_state_id_t current_state_id = 0;
+
+    FSMImpl& impl() { return static_cast<FSMImpl&>(*this); }
+
+public:
+    template<typename StatePack>
+    void set_states() {
+        static_assert(etl::is_same<FSMImpl, typename StatePack::FSMType>::value,
+                      "StatePack FSMType must match tick_fsm FSMImpl type");
+
+        enter_table = StatePack::get_enter_table();
+        run_table = StatePack::get_run_table();
+        exit_table = StatePack::get_exit_table();
+        state_count = StatePack::get_state_count();
+
+        current_state_id = 0;
+        if (enter_table && state_count > 0) {
+            etl::fsm_state_id_t result = enter_table[0](impl());
+            if (result < state_count && result != current_state_id) {
+                change_state(result);
+            }
+        }
+    }
+
+    etl::fsm_state_id_t get_state_id() const {
+        return current_state_id;
+    }
+
+    void run() {
+        if (current_state_id >= state_count) return;
+
+        etl::fsm_state_id_t result = run_table[current_state_id](impl());
+
+        if (result == etl::ifsm_state::Self_Transition) {
+            exit_table[current_state_id](impl());
+            etl::fsm_state_id_t enter_result = enter_table[current_state_id](impl());
+            if (enter_result < state_count && enter_result != current_state_id) {
+                change_state(enter_result);
+            }
+        } else if (result < state_count && result != current_state_id) {
+            change_state(result);
+        }
+    }
+
+    void change_state(etl::fsm_state_id_t new_state_id) {
+        if (new_state_id >= state_count) return;
+
+        etl::fsm_state_id_t next_state_id = new_state_id;
+
+        do {
+            exit_table[current_state_id](impl());
+            current_state_id = next_state_id;
+
+            etl::fsm_state_id_t result = enter_table[current_state_id](impl());
+            next_state_id = (result < state_count) ? result : current_state_id;
+
+        } while (next_state_id != current_state_id);
+    }
+
+    template<typename E, typename = typename etl::enable_if<!etl::is_same<E, etl::fsm_state_id_t>::value>::type>
+    void change_state(E e) { change_state(static_cast<etl::fsm_state_id_t>(e)); }
+};
+
+} // namespace etl_ext
---
+++ test/test_tick_fsm/test_tick_fsm.cpp
@@ -0,0 +1,135 @@
+#include <gtest/gtest.h>
+#include <array>
+#include "utils/tick_fsm.h"
+
+using etl_ext::tick_fsm;
+using etl_ext::tick_fsm_state;
+using etl_ext::tick_fsm_state_pack;
+
+enum StateID : etl::fsm_state_id_t { SID0 = 0, SID1, SID2, SID3, SID4, SID_Count };
+static constexpr etl::fsm_state_id_t InvalidID = SID_Count;
+
+struct TestFSM : tick_fsm<TestFSM> {
+  std::array<int, SID_Count> enter_cnt{};
+  std::array<int, SID_Count> exit_cnt{};
+  std::array<int, SID_Count> run_cnt{};
+  bool self_used = false;
+  bool force_invalid = false;
+};
+
+// S0: after 2 ticks -> S1
+struct S0 : tick_fsm_state<TestFSM, S0, SID0> {
+  static etl::fsm_state_id_t on_enter_state(FSMType& f) { f.enter_cnt[STATE_ID]++; return No_State_Change; }
+  static etl::fsm_state_id_t on_run_state  (FSMType& f) {
+    f.run_cnt[STATE_ID]++;
+    return (f.run_cnt[STATE_ID] == 2) ? ID(SID1) : No_State_Change;
+  }
+  static void on_exit_state(FSMType& f) { f.exit_cnt[STATE_ID]++; }
+};
+
+// S1: first tick -> Self_Transition (re-enter), next tick -> S2
+struct S1 : tick_fsm_state<TestFSM, S1, SID1> {
+  static etl::fsm_state_id_t on_enter_state(FSMType& f) { f.enter_cnt[STATE_ID]++; return No_State_Change; }
+  static etl::fsm_state_id_t on_run_state  (FSMType& f) {
+    f.run_cnt[STATE_ID]++;
+    if (!f.self_used) { f.self_used = true; return Self_Transition; }
+    return SID2;
+  }
+  static void on_exit_state(FSMType& f) { f.exit_cnt[STATE_ID]++; }
+};
+
+// S2: enter() chains to S3
+struct S2 : tick_fsm_state<TestFSM, S2, SID2> {
+  static etl::fsm_state_id_t on_enter_state(FSMType& f) { f.enter_cnt[STATE_ID]++; return SID3; }
+  static etl::fsm_state_id_t on_run_state  (FSMType& f) { f.run_cnt[STATE_ID]++; return No_State_Change; }
+  static void on_exit_state(FSMType& f) { f.exit_cnt[STATE_ID]++; }
+};
+
+// S3: run() may return invalid id
+struct S3 : tick_fsm_state<TestFSM, S3, SID3> {
+  static etl::fsm_state_id_t on_enter_state(FSMType& f) { f.enter_cnt[STATE_ID]++; return No_State_Change; }
+  static etl::fsm_state_id_t on_run_state  (FSMType& f) {
+    f.run_cnt[STATE_ID]++;
+    return f.force_invalid ? InvalidID : No_State_Change;
+  }
+  static void on_exit_state(FSMType& f) { f.exit_cnt[STATE_ID]++; }
+};
+
+// S4: enter() returns Self_Transition (must be ignored)
+struct S4 : tick_fsm_state<TestFSM, S4, SID4> {
+  static etl::fsm_state_id_t on_enter_state(FSMType& f) { f.enter_cnt[STATE_ID]++; return Self_Transition; }
+  static etl::fsm_state_id_t on_run_state  (FSMType& f) { f.run_cnt[STATE_ID]++; return No_State_Change; }
+  static void on_exit_state(FSMType& f) { f.exit_cnt[STATE_ID]++; }
+};
+
+using Pack = tick_fsm_state_pack<S0, S1, S2, S3, S4>;
+
+TEST(TickFsm, InitAndBasicTransition) {
+  TestFSM fsm;
+  fsm.set_states<Pack>();
+  EXPECT_EQ(fsm.get_state_id(), S0::STATE_ID);
+  EXPECT_EQ(fsm.enter_cnt[S0::STATE_ID], 1);
+
+  fsm.run(); // S0 tick 1
+  EXPECT_EQ(fsm.get_state_id(), S0::STATE_ID);
+  fsm.run(); // S0 tick 2 -> S1
+  EXPECT_EQ(fsm.get_state_id(), S1::STATE_ID);
+  EXPECT_EQ(fsm.exit_cnt[S0::STATE_ID], 1);
+  EXPECT_EQ(fsm.enter_cnt[S1::STATE_ID], 1);
+}
+
+TEST(TickFsm, SelfTransitionReenterFromRun) {
+  TestFSM fsm;
+  fsm.set_states<Pack>();
+  fsm.run(); // S0 #1
+  fsm.run(); // S0 #2 -> S1
+
+  fsm.run(); // S1 first run => Self_Transition (exit1, enter1)
+  EXPECT_EQ(fsm.get_state_id(), S1::STATE_ID);
+  EXPECT_EQ(fsm.exit_cnt[S1::STATE_ID], 1);
+  EXPECT_EQ(fsm.enter_cnt[S1::STATE_ID], 2);
+}
+
+TEST(TickFsm, EnterChainingWorks) {
+  TestFSM fsm;
+  fsm.set_states<Pack>();
+  fsm.run(); // S0 #1
+  fsm.run(); // S0 #2 -> S1
+  fsm.run(); // S1 self
+  fsm.run(); // S1 -> S2 (enter chains to S3)
+
+  EXPECT_EQ(fsm.get_state_id(), S3::STATE_ID);
+  EXPECT_EQ(fsm.enter_cnt[S2::STATE_ID], 1);
+  EXPECT_EQ(fsm.exit_cnt[S2::STATE_ID], 1);
+  EXPECT_EQ(fsm.enter_cnt[S3::STATE_ID], 1);
+}
+
+TEST(TickFsm, InvalidIdAndSelfFromEnterAreIgnored) {
+  TestFSM fsm;
+  fsm.set_states<Pack>();
+  fsm.change_state(S3::STATE_ID);
+  EXPECT_EQ(fsm.get_state_id(), S3::STATE_ID);
+
+  fsm.force_invalid = true;
+  fsm.run(); // returns InvalidID -> ignored
+  EXPECT_EQ(fsm.get_state_id(), S3::STATE_ID);
+
+  fsm.change_state(S4::STATE_ID); // enter returns Self -> ignored
+  EXPECT_EQ(fsm.get_state_id(), S4::STATE_ID);
+  EXPECT_EQ(fsm.enter_cnt[S4::STATE_ID], 1);
+  EXPECT_EQ(fsm.exit_cnt[S4::STATE_ID], 0);
+}
+
+TEST(TickFsm, EnumOverloadChangeState) {
+  TestFSM fsm;
+  fsm.set_states<Pack>();
+  fsm.change_state(S4::STATE_ID);
+  EXPECT_EQ(fsm.get_state_id(), S4::STATE_ID);
+  fsm.change_state(S0::STATE_ID);
+  EXPECT_EQ(fsm.get_state_id(), S0::STATE_ID);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
