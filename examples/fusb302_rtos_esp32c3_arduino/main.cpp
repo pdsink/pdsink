6689cdf79b1990cdd347ca2edfc9b0990765dda3
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2025-08-21 20:11:41 +0300

    sample/dpm cleanup

--- examples/fusb302_rtos_esp32c3_arduino/main.cpp
+++ examples/fusb302_rtos_esp32c3_arduino/main.cpp
@@ -2,18 +2,14 @@
 #include "logger.hpp"
 #include "blinker.hpp"
 
-// For HW with disconnected VBUS pin, should work for sink with "dead battery".
-// This may be useful for EPR mode to keep HW simple, because fusb302b allows
-// 28v max at VBUS pin, and may be damaged.
-//
-// But it's better not to use this hack, and connect VBUS pin using resistor
-// and TVS instead. Without EPR, direct VBUS connection will be fine.
 class Driver : public pd::fusb302::Fusb302Rtos {
 public:
-    Driver(pd::Port& port, pd::fusb302::Fusb302RtosHalEsp32& hal)
-        : Fusb302Rtos(port, hal) {}
-
-    bool is_vbus_ok() override { return true; }
+    Driver(pd::Port& port, pd::fusb302::Fusb302RtosHalEsp32& hal) : Fusb302Rtos(port, hal) {
+        // Example of default settings override. You can do the same
+        // for HAL class to reassign IO pins and so on.
+        task_stack_size_bytes = 1024 * 6;
+        task_priority = 7;
+    }
 };
 
 
@@ -36,20 +32,10 @@ void setup() {
     // LED animation example
     blinker.loop({ {{255,0,0}, 1000}, {{0,255,0}, 1000}, {{0,0,255}, 1000}, {{128,128,128}, 1000} });
 
-    //
-    // Devboard specific setup, not PD related
-    //
-
-    // FAN off.
-    pinMode(21, OUTPUT); digitalWrite(21, LOW);
-    // BUZZER off.
-    pinMode(10, OUTPUT); digitalWrite(10, LOW);
-    pinMode(8, OUTPUT); digitalWrite(8, LOW);
-    // Heater off.
-    pinMode(20, OUTPUT); digitalWrite(20, LOW);
-
     // Activate PD stack
     task.start(tc, dpm, pe, prl, driver);
+    // Preset desired voltage
+    dpm.trigger_fixed(12000);
 
     LOG_INFO("Setup complete");
 }
--- src/dpm.h
+++ src/dpm.h
@@ -30,11 +30,10 @@ public:
         //port.msgbus.subscribe(dpm_event_listener);
     }
 
+    //
     // Methods below provide default implementation of DPM logic
     // and can be customized
     //
-
-    virtual bool has_usb_comm() { return false; }
 
     // This one is called from `SRC Capabilities` and `EPR SRC Capabilities`
     // Override with your custom logic to evaluate capabilities and return
@@ -49,14 +48,14 @@ public:
     // Required by spec to enter EPR mode (no ideas why)
     virtual uint32_t get_epr_watts() override { return 140; }
 
-    // Helper to fill request RDO flags
-    virtual void fill_rdo_flags(uint32_t &rdo);
-
     //
     // Sugar methods for simple mode select. You are not forced to use those.
     //
     void trigger_fixed(uint32_t mv);
     void trigger_spr_pps(uint32_t mv, uint32_t ma);
+
+protected:
+    Port& port;
 
     enum class TRIGGER_MODE {
         INACTIVE,
@@ -69,8 +68,10 @@ public:
     // SNK PDOs cache, to build only once
     PDO_LIST sink_pdo_list;
 
-private:
-    Port& port;
+    // Helper to fill request RDO flags
+    virtual void fill_rdo_flags(uint32_t &rdo);
+
+    virtual bool has_usb_comm() { return false; }
 };
 
 } // namespace pd
